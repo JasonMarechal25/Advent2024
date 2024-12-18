8a656dd66b6a551cec55fa14a1dd5c9742055a42
Jason Marechal <jason.marechal@gmail.com>
2024-12-18 18:01:50 +0100

    Day1 part 2

--- day01.h
+++ day01.h
@@ -6,8 +6,8 @@
 #include <boost/algorithm/string.hpp>
 
 struct pair {
-    std::vector<int> list1;
-    std::vector<int> list2;
+    std::vector<int> leftList;
+    std::vector<int> rightList;
 };
 
 
@@ -18,18 +18,35 @@ auto read_day01_input() -> pair {
     pair p;
     while (std::getline(file, line)) {
         boost::split(result, line, boost::is_any_of("\t "), boost::token_compress_on);
-        p.list1.push_back(std::stoi(result[0]));
-        p.list2.push_back(std::stoi(result[1]));
+        p.leftList.push_back(std::stoi(result[0]));
+        p.rightList.push_back(std::stoi(result[1]));
     }
-    std::ranges::sort(p.list1);
-    std::ranges::sort(p.list2);
+    std::ranges::sort(p.leftList);
+    std::ranges::sort(p.rightList);
     return p;
 }
 
 auto compute_distance(const pair& p) -> int {
     int distance = 0;
-    for (int i = 0; i < p.list1.size(); i++) {
-        distance += std::abs(p.list1[i] - p.list2[i]);
+    for (int i = 0; i < p.leftList.size(); i++) {
+        distance += std::abs(p.leftList[i] - p.rightList[i]);
     }
     return distance;
+}
+
+auto computeSimilarity(const pair& sortedPair) -> int {
+    int similarity = 0;
+    int leftIndex = 0;
+    int rightIndex = 0;
+    while (leftIndex < sortedPair.leftList.size()) {
+        if (sortedPair.leftList[leftIndex] == sortedPair.rightList[rightIndex]) {
+            similarity += sortedPair.leftList[leftIndex];
+            ++rightIndex;
+        } else if (sortedPair.leftList[leftIndex] < sortedPair.rightList[rightIndex]) {
+            ++leftIndex;
+        } else { //Happens if right list smallest number is smaller than left list smallest number
+            ++rightIndex;
+        }
+    }
+    return similarity;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <chrono>
 #include "day01.h"
+
+class Timer {
+public:
+    Timer() :
+            m_beg(clock_::now()) {
+    }
+
+    void reset() {
+        m_beg = clock_::now();
+    }
+
+    double elapsed() const {
+        return std::chrono::duration_cast<std::chrono::microseconds >(
+                clock_::now() - m_beg).count();
+    }
+
+private:
+    typedef std::chrono::high_resolution_clock clock_;
+    typedef std::chrono::duration<double, std::ratio<1> > second_;
+    std::chrono::time_point<clock_> m_beg;
+};
+
 
 int main() {
     pair p = read_day01_input();
+    Timer t;
     auto distance = compute_distance(p);
+    std::cout << "Time: " << t.elapsed() << "us" << std::endl;
     std::cout << "Distance: " << distance << std::endl;
+    auto similarity = computeSimilarity(p);
+    std::cout << "Similarity: " << similarity << std::endl;
     return 0;
 }
